09b41f5ec04c466add16dbb7947fca3e170d7361
Chun Sun <chunsun978@gmail.com>
2025-04-12 20:13:22 -0400

    Update for arm build/debug

--- gui/CryptoTraderGUI/main.cpp
+++ gui/CryptoTraderGUI/main.cpp
@@ -2,8 +2,16 @@
 // main.cpp
 #include "mainwindow.h"
 #include <QApplication>
+#include <QThread>
+#include <QTimer>
+#include <QDebug>
+
 int main(int argc, char *argv[]) {
+    qDebug() << "Entering main()"; // Debug output
     QApplication app(argc, argv);
+    qDebug() << "QApplication created";
+    QThread::msleep(2000); // 2-second delay
+    qDebug() << "After delay";
     MainWindow w;
     w.show();
     return app.exec();
